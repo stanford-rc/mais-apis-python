86ba5b13175a8b183e41d9ac6da32a5a9d40cab5
A. Karl Kornel <akkornel@stanford.edu>
2021-07-05 18:53:00 -0700

    account: Remove two unused imports

--- src/stanford/mais/account/account.py
+++ src/stanford/mais/account/account.py
@@ -15,10 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import abc
 import dataclasses
 import datetime
-import enum
 import logging
 import requests
 from typing import *
