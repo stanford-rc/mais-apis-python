9b41bde23e16513046357c053c03192440c3a74c
A. Karl Kornel <akkornel@stanford.edu>
2025-08-20 18:02:45 -0700

    account tests: Move properties tests to their own file
    The code which tests account properties is long enough that it should go
    into its own file.  The other tests which were in this file will be
    moved to their own file.

--- tests/test_account_properties.py
+++ tests/test_account_properties.py
@@ -22,51 +22,15 @@ from stanford.mais.account import AccountClient, Account
 from stanford.mais.account.service import ServiceStatus
 
 
-# An AccountClient with no client or a bad client should throw.
-def test_badclient():
-    with pytest.raises(TypeError):
-        AccountClient()
-    with pytest.raises(TypeError):
-        AccountClient(client='blah')
-
 """
-Account:
-- get with non-ASCII raises ValueError
-- return 401/403 raises PermissionError
-- return 400/500 raises ChildProcessError
-- return 404 raises KeyError
+This tests all of the properties that can be returned within an Account.
 
-Account `nøbødy` shouldn't even make an API call.
-Account `nobody` returns a 404
-Account `hidden1` returns a 401
-Account `hidden3` returns a 403
-Account `broken4` returns a 400
-Account `broken5` returns a 500
-"""
-
-def test_badqueries(account_client):
-    with pytest.raises(ValueError):
-        ac = account_client['nøbødy']
-    with pytest.raises(KeyError):
-        ac = account_client['nobody']
-    with pytest.raises(PermissionError):
-        ac = account_client['hidden1']
-    with pytest.raises(PermissionError):
-        ac = account_client['hidden3']
-    with pytest.raises(ChildProcessError):
-        ac = account_client['broken4']
-    with pytest.raises(ChildProcessError):
-        ac = account_client['broken5']
-
-"""
 All test accounts (that aren't intentionally broken) have:
     - sunetid
     - name
     - description
     - last_updated
-"""
 
-"""
 These are our test users:
     fullprsn:
         is_person is True
@@ -132,16 +96,6 @@ These are our test users:
             email
             autoreply
 """
-
-# Test that the get method and the key-based methods work
-# Once we test this, we'll use key-based methods from now on.
-def test_get_vs_key(account_client):
-    key_result = account_client['fullprsn']
-    get_result = Account.get(
-        client=account_client,
-        sunetid='fullprsn',
-    )
-    assert key_result is get_result
 
 # Test that we get the expected SUNetIDs
 def test_sunetid(account_client):
