2162939cc2d40c7bae30a782080cf14b4570bb30
A. Karl Kornel <akkornel@stanford.edu>
2025-08-21 11:37:40 -0700

    account tests: Add tests of account filtering
    This adds tests of the `AccountClient.only_*()` methods, including
    making sure that methods can stack.

--- tests/test_account.py
+++ tests/test_account.py
@@ -65,3 +65,144 @@ def test_get_vs_key(account_client):
         sunetid='fullprsn',
     )
     assert key_result is get_result
+
+"""
+The next set of tests checks that AccountViews work.  It uses our test users:
+
+'fullprsn',
+'frozprsn',
+'formerpsn',
+'affilite',
+'afilbase',
+'functional',
+'oldfunctional',
+'sharedmailbox',
+"""
+
+
+# Test inactive folks cannot be found
+def test_only_active(account_client):
+    only_active = account_client.only_active()
+
+    assert only_active['fullprsn'].sunetid == 'fullprsn'
+    assert only_active['frozprsn'].sunetid == 'frozprsn'
+    with pytest.raises(KeyError):
+        assert only_active['formerpsn'].sunetid == 'formerpsn'
+    assert only_active['affilite'].sunetid == 'affilite'
+    assert only_active['afilbase'].sunetid == 'afilbase'
+    assert only_active['functional'].sunetid == 'functional'
+    with pytest.raises(KeyError):
+        assert only_active['oldfunctional'].sunetid == 'oldfunctional'
+    assert only_active['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# Test that *active* folks cannot be found
+def test_only_inactive(account_client):
+    only_inactive = account_client.only_inactive()
+
+    with pytest.raises(KeyError):
+        assert only_inactive['fullprsn'].sunetid == 'fullprsn'
+    with pytest.raises(KeyError):
+        assert only_inactive['frozprsn'].sunetid == 'frozprsn'
+    assert only_inactive['formerpsn'].sunetid == 'formerpsn'
+    with pytest.raises(KeyError):
+        assert only_inactive['affilite'].sunetid == 'affilite'
+    with pytest.raises(KeyError):
+        assert only_inactive['afilbase'].sunetid == 'afilbase'
+    with pytest.raises(KeyError):
+        assert only_inactive['functional'].sunetid == 'functional'
+    assert only_inactive['oldfunctional'].sunetid == 'oldfunctional'
+    with pytest.raises(KeyError):
+        assert only_inactive['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# Test functional accounts cannot be found
+def test_only_people(account_client):
+    only_people = account_client.only_people()
+
+    assert only_people['fullprsn'].sunetid == 'fullprsn'
+    assert only_people['frozprsn'].sunetid == 'frozprsn'
+    assert only_people['formerpsn'].sunetid == 'formerpsn'
+    assert only_people['affilite'].sunetid == 'affilite'
+    assert only_people['afilbase'].sunetid == 'afilbase'
+    with pytest.raises(KeyError):
+        assert only_people['functional'].sunetid == 'functional'
+    with pytest.raises(KeyError):
+        assert only_people['oldfunctional'].sunetid == 'oldfunctional'
+    with pytest.raises(KeyError):
+        assert only_people['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# Test that people cannot be found
+def test_only_functional(account_client):
+    only_functional = account_client.only_functional()
+
+    with pytest.raises(KeyError):
+        assert only_functional['fullprsn'].sunetid == 'fullprsn'
+    with pytest.raises(KeyError):
+        assert only_functional['frozprsn'].sunetid == 'frozprsn'
+    with pytest.raises(KeyError):
+        assert only_functional['formerpsn'].sunetid == 'formerpsn'
+    with pytest.raises(KeyError):
+        assert only_functional['affilite'].sunetid == 'affilite'
+    with pytest.raises(KeyError):
+        assert only_functional['afilbase'].sunetid == 'afilbase'
+    assert only_functional['functional'].sunetid == 'functional'
+    assert only_functional['oldfunctional'].sunetid == 'oldfunctional'
+    assert only_functional['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# Test stacking views: We only want active people
+def test_active_people(account_client):
+    only_active_people = account_client.only_active().only_people()
+
+    assert only_active_people['fullprsn'].sunetid == 'fullprsn'
+    assert only_active_people['frozprsn'].sunetid == 'frozprsn'
+    with pytest.raises(KeyError):
+        assert only_active_people['formerpsn'].sunetid == 'formerpsn'
+    assert only_active_people['affilite'].sunetid == 'affilite'
+    assert only_active_people['afilbase'].sunetid == 'afilbase'
+    with pytest.raises(KeyError):
+        assert only_active_people['functional'].sunetid == 'functional'
+    with pytest.raises(KeyError):
+        assert only_active_people['oldfunctional'].sunetid == 'oldfunctional'
+    with pytest.raises(KeyError):
+        assert only_active_people['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# Active *and* inactive?  Nobody should be found, then.
+def test_nobody_1(account_client):
+    only_nobody_1 = account_client.only_active().only_inactive()
+
+    with pytest.raises(KeyError):
+        assert only_nobody_1['fullprsn'].sunetid == 'fullprsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['frozprsn'].sunetid == 'frozprsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['formerpsn'].sunetid == 'formerpsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['affilite'].sunetid == 'affilite'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['afilbase'].sunetid == 'afilbase'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['functional'].sunetid == 'functional'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['oldfunctional'].sunetid == 'oldfunctional'
+    with pytest.raises(KeyError):
+        assert only_nobody_1['sharedmailbox'].sunetid == 'sharedmailbox'
+
+# People *and* functional?  Nobody should be found, then.
+def test_nobody_2(account_client):
+    only_nobody_2 = account_client.only_people().only_functional()
+
+    with pytest.raises(KeyError):
+        assert only_nobody_2['fullprsn'].sunetid == 'fullprsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['frozprsn'].sunetid == 'frozprsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['formerpsn'].sunetid == 'formerpsn'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['affilite'].sunetid == 'affilite'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['afilbase'].sunetid == 'afilbase'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['functional'].sunetid == 'functional'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['oldfunctional'].sunetid == 'oldfunctional'
+    with pytest.raises(KeyError):
+        assert only_nobody_2['sharedmailbox'].sunetid == 'sharedmailbox'
