stanford-rc/mais-apis-python

commit 0a1cff45e28b1630676138a367e45cdabd60ae5e

client: Set up an oauth2_session
A major blob of code in support of #49!

To support OAuth-based authentication, we are going to need a separate Session
instance.  Our existing Session is configured to use a client certificate on
all requests, but that won't work with OAuth.

This adds a new `MAISClient` constructor parameter: `oauth2_session`.  It is an
`OAuth2Session`, either set up by us or by the user.

The OAuth2Session is constructed with the user-provided `client_id`,
`client_secret`, and `token_url`.  We hard-code the `client_credentials` grant
type and an access-code expiration leeway of 5 minutes (300 seconds).  If a
custom timeout was provided, we also provide that to the OAuth2Session
constructor.

NOTE: This construction method is slightly different vs. what is
widely-documented.  We provide the `client_id`, `client_secret`,
`token_endpoint`, `grant_type`, and `leeway` parameters to the `OAuth2Session`
constructor.  When we call `fetch_token()`, we call it without any parameters.
By doing things this way, parameters like the grant type and token endpoint URL
get stored into the underlying `OAuth2Client`'s 'metadata'.  And *that* is what
allows client-credential access tokens to be automatically refreshed when they
expire.

Regardless of session-construction method — constructed by us or provided by
the user — if we have an oauth_session we immediately fetch our first access
token.

If there's a problem getting our access token, Authlib will raise an exception.
The documentation for `client_id` and `client_secret` now mentions this.

Speaking of docs, the Authlib docs do not have intersphinx references for
`authlib.integrations.requests_client.OAuthError` or
`authlib.integrations.requests_client.oauth2_session.OAuth2Session`, so we must
add them to the nitpick ignore list.

In terms of testing, since the mais_client fixture now include OAuth client
credentials, that implicitly tests fetching a token.  We use that to test token

src/stanford/mais/client/__init__.py
@@ -33,6 +33,9 @@ import requests
 import ssl
 from typing import NamedTuple, TypedDict
 
+# Next, do third-party imports
+from authlib.integrations.requests_client.oauth2_session import OAuth2Session
+
 # Set up logging
 logger = logging.getLogger(__name__)
 debug = logger.debug
@@ -188,6 +191,8 @@ class MAISClient():
         token.
 
     :raises TypeError: You provided an OAuth Client ID, but no Client Secret.
+
+    :raises authlib.integrations.requests_client.OAuthError: You provided an invalid OAuth Client ID.
     """
 
     client_secret: str | None = None
@@ -202,6 +207,8 @@ class MAISClient():
         token.
 
     :raises TypeError: You provided an OAuth Client Secret, but no Client ID.
+
+    :raises authlib.integrations.requests_client.OAuthError: You provided an invalid OAuth Client Secret.
     """
 
     cert: os.PathLike | None = None
@@ -285,6 +292,24 @@ class MAISClient():
     it, and the ``cert``, ``key``, and ``timeout`` parameters are
     ignored.  You must also set the `Accept` header to `application/json`, and
     you should also set the `User-Agent` header.
+
+    This Session will be used for APIs that use certificate authentication.
+    """
+
+    oauth2_session: OAuth2Session | None = dataclasses.field(default=None, repr=False, init=False)
+    """The OAuth2Session to use for OAuth2-auth API requests.
+
+    In most cases, you should not provide an OAuth2Session during instance
+    creation.  The default behavior is to let the class constructor create the
+    OAuth2Session, using the ``client_id`` and ``default_timeout`` parameters.
+    Automatic token refresh is configured, along with some headers.
+
+    If you provide your own OAuth2Session, then you are responsible for
+    configuring it, and the ``client_id`` and ``timeout`` parameters are
+    ignored.  You must also set the `Accept` header to `application/json`, and
+    you should also set the `User-Agent` header.
+
+    This OAuth2Session will be used for APIs that use OAuth 2.0 authentication.
     """
 
     timeout: Timeout | float | None = None
@@ -380,6 +405,63 @@ class MAISClient():
                 object.__setattr__(self, 'session', new_session)
         else:
             debug('Using client-provided session')
+
+        # Do we need to create our own OAuth2Session?
+        if self.oauth2_session is None:
+            # Only create an OAuth2Session if we have a Client ID
+            if self.client_id is None:
+                debug('No Client ID - Skipping oauth2_session creation')
+            else:
+                debug('Creating OAuth2Session')
+
+                # Prepare our OAuth2Session constructor arguments
+                # NOTE: Most Authlib documentation tells you to provide the
+                # token endpoint and grant type when you are making the
+                # `fetch_token` call.  Instead, you should provide it as part
+                # of the OAuth2Session constructor.
+                #
+                # If you provide the token_endpoint and grant_type here, they
+                # are passed through to the underlying OAuth2Client
+                # constructor.  *That* constructor will store them as metadata,
+                # and the metadata is used when refreshing expired access
+                # tokens!
+                oauth2_session_args = {
+                    'client_id': self.client_id,
+                    'client_secret': self.client_secret,
+                    'token_endpoint': self.token_url,
+                    'grant_type': 'client_credentials',
+                    'leeway': 300, # 5 minutes
+                }
+
+                # If we have a timeout, add it
+                # NOTE: Typeshed does not have a type for default_timeout.
+                if self.timeout is not None:
+                    oauth2_session_args['default_timeout'] = self.timeout # type: ignore[assignment]
+
+                # Make our session, with or without a timeout
+                new_oauth2_session = OAuth2Session(
+                    **oauth2_session_args
+                )
+
+                # Ask for JSON responses
+                new_oauth2_session.headers.update({ # type: ignore[attr-defined]
+                    'Accept': 'application/json',
+                })
+
+                # Set our custom user-agent
+                new_oauth2_session.headers.update({ # type: ignore[attr-defined]
+                    'User-Agent': USER_AGENT,
+                })
+
+                # Finally, put the new session into place
+                object.__setattr__(self, 'oauth2_session', new_oauth2_session)
+        else:
+            debug('Using client-provided oauth2_session')
+
+        # If using OAuth, fetch our first token
+        if self.oauth2_session is not None:
+            debug('Fetching initial access token')
+            self.oauth2_session.fetch_token()
 
         # That's it!
         return None

docs/conf.py
@@ -68,6 +68,8 @@ nitpicky = True
 
 # Some references do not exist, so we ignore them
 nitpick_ignore = [
+    ('py:exc', 'authlib.integrations.requests_client.OAuthError'),
+    ('py:class', 'authlib.integrations.requests_client.oauth2_session.OAuth2Session'),
 ]
 
 # -- Options for HTML output -------------------------------------------------

tests/test_client.py
@@ -20,7 +20,10 @@ import itertools
 import platform
 import pytest
 import ssl
+import time
+from authlib.integrations.requests_client import OAuthError
 from stanford.mais.client import MAISClient
+from stanford.mais.client.tests.mock import add_token_responses
 
 services = (
     'account',
@@ -202,6 +205,76 @@ def test_missing_client_credentials():
             client_secret='blah',
         )
 
+# Test that our token auto-refreshes correctly
+def test_oauth_refresh(mais_client):
+    oauth2_session = mais_client.oauth2_session
+
+    # Our token should already be active
+    assert oauth2_session.ensure_active_token() is True
+
+    # Make a note of the current access token, then change it
+    old_access_token = oauth2_session.token['access_token']
+    oauth2_session.token['access_token'] = 'x'
+
+    # Change the expiration time, and ensure it's expired
+    old_expires_at = oauth2_session.token['expires_at']
+    oauth2_session.token['expires_at'] = 1
+    assert oauth2_session.token.is_expired() is True
+
+    # Sleep two seconds, to ensure expiration times are different
+    time.sleep(2)
+
+    # Check for an active token; this triggers the refresh
+    assert oauth2_session.ensure_active_token() is True
+
+    # Check the token has actually refreshed
+    assert oauth2_session.token['access_token'] == old_access_token
+    assert oauth2_session.token['expires_at'] != old_expires_at
+
+# Test for an invalid Client ID
+def test_bad_clent_id():
+    # Normally we add responses mocks inside a PyTest fixture.  But we cannot
+    # do that here.  Why?  Because we need to test for an exception that is
+    # raised during the constructor.
+    add_token_responses()
+
+    with pytest.raises(OAuthError):
+        mais_client = MAISClient(
+            urls={
+                'account': {
+                    'cert': 'http://example.com/accounts/',
+                },
+                'workgroup': {
+                    'cert': 'http://example.com/wg/v2/',
+                    'oauth': 'http://example.com/oauth/wg/v2',
+                },
+            },
+            token_url='http://example.com/oauth2/token',
+            client_id='bad',
+            client_secret='good',
+        )
+
+def test_bad_client_secret():
+    # Normally we add responses mocks inside a PyTest fixture.  But we cannot
+    # do that here.  Why?  Because we need to test for an exception that is
+    # raised during the constructor.
+    add_token_responses()
+
+    with pytest.raises(OAuthError):
+        mais_client = MAISClient(
+            urls={
+                'account': {
+                    'cert': 'http://example.com/accounts/',
+                },
+                'workgroup': {
+                    'cert': 'http://example.com/wg/v2/',
+                    'oauth': 'http://example.com/oauth/wg/v2',
+                },
+            },
+            token_url='http://example.com/oauth2/token',
+            client_id='good',
+            client_secret='bad',
+        )
 
 # Making a MAISClient that has no auth at all should succeed.
 def test_no_auth():

commit c3dfb771ac7e66e474a9515b57be52075cddac54

Suppress Bandit B105/B106 false-positives
Recent commits (see the list at the end of the commit message) introduced
several instances of Bandit warnings B105 ("hardcoded_password_string") and
B106 ("hardcoded_password_funcarg").  All of them are false-positives:

* All the instances inside the `tests` directory, along with all instances
  inside `src/stanford/mais/client/tests/mock.py`, are coming from OAuth 2.0
  client secrets & access tokens that are used for testing.

* All the instances inside `src/stanford/mais/client/__init__.py` are coming
  from the `client_secret` parameter name.

I'm addressing these issues in a few ways:

1. For `src/stanford/mais/client/__init__.py`, `tests/test_account.py`, and
   `tests/test_workgroup.py`, the number of instances is small, so I've added
   specific `nosec` comments on each instance.

2. For the other affected files, there are enough alerts that I'd rather just
   disable the appropriate Bandit plugin (general_hardcoded_password) for those
   files.

Unfortunately, (2) is not doable because that Bandit plugin does not support
plugin-specific configuration.  Contrast that with the assert_used plugin,
which does have a plugin-specific configuration.

Since there's no plugin-specific configuration for the
general_hardcoded_password plugin, I'm telling Bandit to skip checking some
test files entirely.

See-also: PyCQA/bandit#982
Fixes: 0a1cff45e28 ("client: Set up an oauth2_session")
Fixes: 10688a0a57b ("tests/conftest: Add OAuth to mais_client")
Fixes: 5a0ad125d0e ("Support for providing client_id and client_secret")
Fixes: 6ddceab54d0 ("client: Mock Authorization Server Token Endpoint")
Assisted-by: Claude-5-sonnet
Assisted-by: Claude-4.8-opus

pyproject.toml
@@ -154,5 +154,19 @@ commands = [
 "3.13" = ["py313-minimal", "py313-latest", "report"]
 "3.14" = ["py314-minimal", "py314-latest", "report"]
 
+[tool.bandit]
+exclude_dirs = [
+    # These three files are test-related files that are raising B105 & B106
+    # (hardcoded_password_string and hardcoded_password_funcarg).  That makes
+    # sense, as we're hardcoding OAuth 2.0 client credentials & tokens in
+    # tests.  Problem is, the general_hardcoded_password plugin doesn't support
+    # the same method of configuration as the assert_used plugin.  So, we have
+    # to ignore the files entirely.
+    # See https://github.com/PyCQA/bandit/issues/982
+    "src/stanford/mais/client/tests/mock.py",
+    "tests/conftest.py",
+    "tests/test_client.py",
+]
+
 [tool.bandit.assert_used]
 skips = [ "tests/*.py" ]

src/stanford/mais/client/__init__.py
@@ -501,7 +501,7 @@ class MAISClient():
         :raises ssl.SSLError: The private key and certificate do not match, or
             there was some other problem loading the certificate.
         """
-        return cls(
+        return cls( # nosec: B106
             urls={
                 'account': {
                     'cert': 'https://accountws.stanford.edu/accounts/',
@@ -566,7 +566,7 @@ class MAISClient():
         :raises ssl.SSLError: The private key and certificate do not match, or
             there was some other problem loading the certificate.
         """
-        return cls(
+        return cls( # nosec: B106
             urls={
                 'account': {
                     'cert': 'https://accountws-uat.stanford.edu/accounts/',

tests/test_account.py
@@ -35,7 +35,7 @@ def test_noauthclient():
 
 def test_authmismatch(snakeoil_cert):
     # Make a client with only a cert endpoint
-    mais_client = MAISClient(
+    mais_client = MAISClient( # nosec: B106
         urls={
             'account': {
                 'cert': 'http://example.com/accounts/',

tests/test_workgroup.py
@@ -42,7 +42,7 @@ def test_noauthclient():
 
 def test_authmismatch(snakeoil_cert):
     # Make a client with only a cert endpoint
-    mais_client = MAISClient(
+    mais_client = MAISClient( # nosec: B106
         urls={
             'workgroup': {
                 'cert': 'http://example.com/wg/v2/',